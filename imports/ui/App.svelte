ec9dda1df81ffa764f4ae9037e2ae441aa15af59
Manassarn Manoonchai <manassarn@gmail.com>
2022-06-27 22:22:55 +0700

    Initial commit

---
+++ imports/ui/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Task from "./Task.svelte";
+
+  const getTasks = () => [
+    { _id: "task_1", text: "This is task 1" },
+    { _id: "task_2", text: "This is task 2" },
+    { _id: "task_3", text: "This is task 3" },
+  ];
+</script>
+
+<div class="container">
+  <h1>Todo in Meteor & Svelte</h1>
+
+  <ul>
+    {#each getTasks() as task (task._id)}
+      <Task {task} />
+    {/each}
+  </ul>
+</div>
